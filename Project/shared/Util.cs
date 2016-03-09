59db9179afc1879207ae63835fa8cdc13dbce5dc
duongnhyt <duongnhyt@gmail.com>
2016-03-09 09:58:24 +0700

    Hoàn thành Chức năng thêm mới hồ sơ

--- Project/shared/Util.cs
+++ Project/shared/Util.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI;
 
 namespace Project.shared
 {
     public class Util
     {
+        public static void ShowExceptionError(Exception ex, Page p)
+        {
+            p.Response.Write(ex.ToString());
+        }
     }
 }
--- Project/views/hoso/FormThemHS.aspx.cs
+++ Project/views/hoso/FormThemHS.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project.shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,74 @@ namespace Project.views
 {
     public partial class FormThemHS : System.Web.UI.Page
     {
+        DatabaseDataContext db = new DatabaseDataContext();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                //lấy dữ liệu cho ddl_CQLuuTru
+                ddl_CQLuuTru.DataSource = db.Khos;
+                ddl_CQLuuTru.DataTextField = "TenKho";
+                ddl_CQLuuTru.DataValueField = "MaKho";
+                ddl_CQLuuTru.DataBind();
 
+                //lấy dữ liệu cho ddl_phong
+                ddl_Phong.DataSource = db.Phongs;
+                ddl_Phong.DataTextField = "TenPhong";
+                ddl_Phong.DataValueField = "MaPhong";
+                ddl_Phong.DataBind();
+            }
+        }
+
+        protected void btn_themmoi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Hsrecord obj = new Hsrecord();
+                //int rsTryParse = 0;
+                obj.MaKho = int.Parse(ddl_CQLuuTru.SelectedValue);
+                obj.MaPhong = int.Parse(ddl_Phong.SelectedValue);
+                obj.Muclucso = ddl_MucLucSo.SelectedValue;
+                obj.Hopso = txt_HopSo.Text;
+                obj.Hososo = txt_HSSo.Text;
+                obj.Ngonngu = ddl_NgonNgu.SelectedValue;
+                obj.KHTT = txt_KHTT.Text;
+                obj.Tieude = txt_TDHS.Text;
+                obj.Chugiai = txta_ChuGiai.Text;
+                obj.ThoigianBD = txt_TGBD.Text;
+                obj.ThoigianKT = txt_TGKT.Text;
+                obj.Buttich = txt_Buttich.Text;
+                obj.Soluongto = txt_SoLuongTu.Text;
+                obj.ThoihanBQ = ddl_TGBaoquan.SelectedValue;
+                obj.ChedoSD = ddl_CDSD.SelectedValue;
+                obj.TinhtrangVL = ddl_TTVL.SelectedValue;
+
+                db.Hsrecords.InsertOnSubmit(obj);
+
+                db.SubmitChanges();
+
+                Response.Write("Thành công");
+            }
+            catch (Exception ex)
+            {
+                Util.ShowExceptionError(ex, this);
+                throw;
+            }
+            
+        }
+
+        protected void btn_huybo_Click(object sender, EventArgs e)
+        {
+            txta_ChuGiai.Text = "";
+            txt_Buttich.Text = "";
+            txt_HopSo.Text = "";
+            txt_HSSo.Text = "";
+            txt_KHTT.Text = "";
+            txt_SoLuongTu.Text = "";
+            txt_TDHS.Text = "";
+            txt_TGBD.Text = "";
+            txt_TGKT.Text = "";
         }
     }
 }
