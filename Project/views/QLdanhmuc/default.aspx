db51fa57598d5af2ebc1ca0cc29cef4341bef60f
Nguyễn Văn Toàn <toanthree@gmail.com>
2016-03-09 10:51:47 +0700

    Code chuc nang quan ly danh muc

---
+++ Project/views/QLdanhmuc/default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/views/layout/Site.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Project.views.QLdanhmuc._default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="row">
+                    <div class="col-sm-11  div_content">
+                        <div class="row nav_bar header_padding text-center">Quản lý danh mục</div>
+                        <br />
+
+                        <div class="row margin_b1">
+                            <div class="list-group font_bold ">
+                                <a href="/views/phong/FormThemPhong.aspx" class="list-group-item">Quản lý Phông lưu trữ</a>
+                                <a href="#" class="list-group-item">Quản lý Mục lục</a>
+                                <a href="#" class="list-group-item">Quản lý danh mục Loại văn bản</a>
+                                <a href="#" class="list-group-item">Quản lý danh mục Thời hạn bảo quản</a>
+                                <a href="#" class="list-group-item">Quản lý danh mục Chế độ sử dụng</a>
+                                <a href="#" class="list-group-item">Quản lý danh mục Tình trạng vật lý</a>
+                                <a href="#" class="list-group-item">Quản lý danh mục Ngôn ngữ</a>
+                                <a href="#" class="list-group-item">Quản lý danh mục Đơn vị hành chính</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-9 div_content" style="padding: 20px 0 0 0">
+            </div>
+
+
+        </div>
+</asp:Content>
--- Project/views/layout/Site.Master.cs
+++ Project/views/layout/Site.Master.cs
@@ -11,6 +11,78 @@ namespace Project.views.layout
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //[Toàn] class active for navigation
+            string li_item = Request.QueryString["active"];
+            if (li_item  != "" && li_item != null)
+            {
+                switch (li_item)
+                {
+                    case "DM":
+                        Li_DM.Attributes.Add("class", "active");
+                        break;
+
+                    case "TT":
+                        Li_TT.Attributes.Add("class", "active");
+                        break;
+
+                    case "HT":
+                        Li_HT.Attributes.Add("class", "active");
+                        break;
+
+                    case "GT":
+                        Li_GT.Attributes.Add("class", "active");
+                        break;
+
+                    case "LT":
+                        Li_LT.Attributes.Add("class", "active");
+                        break;
+
+                    case "BC":
+                        Li_BC.Attributes.Add("class", "active");
+                        break;
+                    default:
+                        break;
+                }
+            }
+     
+        }
+
+        protected void a_QLDM_Click(object sender, EventArgs e)
+        {
+
+            //[Toàn] class active for navigation
+            LinkButton button = (LinkButton)sender;
+            string buttonId = button.ID;
+
+            switch (buttonId)
+            {
+                case "Link_TT":
+                    Response.Redirect("/?active=TT");
+
+                    break;
+                case "Link_HT":
+                    Response.Redirect("/views/QLdanhmuc/default.aspx?active=HT");
+
+                    break;
+                case "Link_GT":
+                    Response.Redirect("/views/QLdanhmuc/default.aspx?active=GT");
+
+                    break;
+                case "Link_DM":
+                    Response.Redirect("/views/QLdanhmuc/default.aspx?active=DM");
+
+                    break;
+                case "Link_LT":
+                    Response.Redirect("/views/QLdanhmuc/default.aspx?active=LT");
+
+                    break;
+                case "Link_BC":
+                    Response.Redirect("/views/QLdanhmuc/default.aspx?active=BC");
+
+                    break;
+                default:
+                    break;
+            }
 
         }
     }
